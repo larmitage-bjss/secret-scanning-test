04ac69ef355a15a3e86ad35a66a0507e83433ee4
Laura Armitage <laura.armitage@bjss.com>
2024-01-24 09:33:14 +0000

    Change all files to trigger scans

--- index.js
+++ index.js
@@ -19,3 +19,24 @@ var rsaPrivateKey = "-----BEGIN RSA PRIVATE KEY-----"
 
 var dummyDbConnStr = "Driver=postgresql;Server=192.168.0.1;Database=my-database;Uid=myusername;Pwd=thisisasillyinsecurepassword;";
 
+var myFakePassword = "XpRTh78mLB$sy2q6sCWbZfhGP5W%ufU^";
+var password2 = "CrvR9#61heT~152?";
+
+var fakeAwsSecret = "DcCc9H6oCkGUSp3Rhmsx8NIfVG8kO2T/3jORxuZY";
+
+var azure_sql_password = "DcCc9H6oCkGUSp3Rhmsx8NIfVG8kO2T/3jORxuZY";
+
+var fakeApiKey = "apikey1234abcdefghij0123456789";
+
+var rsaPrivateKey = "-----BEGIN RSA PRIVATE KEY-----"
++ "MIIBOgIBAAJBAKj34GkxFhD90vcNLYLInFEX6Ppy1tPf9Cnzj4p4WGeKLs1Pt8Qu"
++ "KUpRKfFLfRYC9AIKjbJTWit+CqvjWYzvQwECAwEAAQJAIJLixBy2qpFoS4DSmoEm"
++ "o3qGy0t6z09AIJtH+5OeRV1be+N4cDYJKffGzDa88vQENZiRm0GRq6a+HPGQMd2k"
++ "TQIhAKMSvzIBnni7ot/OSie2TmJLY4SwTQAevXysE2RbFDYdAiEBCUEaRQnMnbp7"
++ "9mxDXDf6AU0cN/RPBjb9qSHDcWZHGzUCIG2Es59z8ugGrDY+pxLQnwfotadxd+Uy"
++ "v/Ow5T0q5gIJAiEAyS4RaI9YG8EWx/2w0T67ZUVAw8eOMB6BIUg0Xcu+3okCIBOs"
++ "/5OiPgoTdSy7bcF9IGpSE8ZgGKzgYQVZeN97YE00"
++ "-----END RSA PRIVATE KEY-----";
+
+var dummyDbConnStr = "Driver=postgresql;Server=192.168.0.1;Database=my-database;Uid=myusername;Pwd=thisisasillyinsecurepassword;";
+
